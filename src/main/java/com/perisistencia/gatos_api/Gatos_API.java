fb8c6aecbf4677c053df403f2131d9b1e1885403
SergioFS_Ing07 <sefernandez0711@gmail.com>
2023-04-04 15:50:12 -0500

    En la clase principal, creo el panel de la App

--- src/main/java/com/perisistencia/gatos_api/Gatos_API.java
+++ src/main/java/com/perisistencia/gatos_api/Gatos_API.java
@@ -4,6 +4,8 @@
 
 package com.perisistencia.gatos_api;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author USER
@@ -11,6 +13,34 @@ package com.perisistencia.gatos_api;
 public class Gatos_API {
 
     public static void main(String[] args) {
+        
+        int opcion_menu = -1; 
+        String[] botones = {"1. Ver gatos", "2. Salir"}; 
+        
+        do {           
+            
+            String opcion = (String) JOptionPane.showInputDialog(null, "Gatitos Java", "Menu principal",
+                    JOptionPane.INFORMATION_MESSAGE, null, botones, botones[0]); 
+            
+            
+            // creamos esta validacion para saber que valor toma la seccion de opcion 
+            for (int i = 0; i < botones.length; i++) {
+                
+                if(opcion.equals(botones[i])) {
+                    
+                    opcion_menu = i; 
+                }    
+            }
+            
+            switch (opcion_menu) {
+                case 0: 
+                    GatosService.verGatos();
+                    break;
+                default:
+                    break;
+            }
+            
+        } while (opcion_menu != 1);
         
         
     }
