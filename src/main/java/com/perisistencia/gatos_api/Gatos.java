2922800454b958e4fee1c1e8e375344cde81b8cf
SergioFS_Ing07 <sefernandez0711@gmail.com>
2023-04-04 15:27:24 -0500

    [Inicio Proyecto Java Api para gatos] - Creo la clase objeto de Gatos

---
+++ src/main/java/com/perisistencia/gatos_api/Gatos.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.perisistencia.gatos_api;
+
+/**
+ *
+ * @author USER
+ */
+public class Gatos {
+    
+    int id;
+    String url;
+    String apikey = "live_XIN3L3cOcvKXlUfBbCs0rnIAlAPat08GZ5f1T1rlQcY3st90JyHc2h12QdiqQZQC";
+    String imagen; 
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getApikey() {
+        return apikey;
+    }
+
+    public void setApikey(String apikey) {
+        this.apikey = apikey;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+    
+    
+    
+}
---
+++ src/main/java/com/perisistencia/gatos_api/Gatos_API.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.perisistencia.gatos_api;
+
+/**
+ *
+ * @author USER
+ */
+public class Gatos_API {
+
+    public static void main(String[] args) {
+        
+        
+    }
+}
